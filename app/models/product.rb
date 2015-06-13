c760d3df6a25b3fbe0a8d24ee2967e31f8754c86
Marius Butuc <marius.butuc@gmail.com>
2015-06-13 07:42:21 -0400

    User can upload files, using Paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ActiveRecord::Base
+  belongs_to :user
+
   has_attached_file :file
 
-  belongs_to :user
+  validates_attachment_content_type :file, content_type: [
+    'image/jpg',
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'application/pdf',
+    'application/zip',
+  ]
 end
